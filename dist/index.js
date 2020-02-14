0d172459ed0c6539b6d0d16e19361c9889300709
Jonas Strassel <jo.strassel@gmail.com>
2020-02-14 11:44:21 +0100

    log ref

--- dist/index.js
+++ dist/index.js
@@ -498,17 +498,19 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 const fetch = __webpack_require__(454);
 
+let owner, repo, pr_number;
 const ghClient = new github.GitHub(process.env.GITHUB_TOKEN);
 
 const rebase = () => {};
 
 const run = async () => {
     const context = await github.context;
-    owner = core.getInput('owner') || context.payload.repository.full_name.split('/')[0];
-    repo = core.getInput('repo') || context.payload.repository.full_name.split('/')[1];
-    console.log(github)
-    console.log(github.context.payload);
-    console.log(github.context.issue())
+    owner = context.payload.repository.full_name.split('/')[0];
+    repo = context.payload.repository.full_name.split('/')[1];
+    issue_number = context.payload.issue.number;
+    console.log(github.ref)
+    console.log(github.base_ref);
+    console.log(github.head_ref);
 };
 
 run();
--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require('node-fetch');
 
+let owner, repo, pr_number;
 const ghClient = new github.GitHub(process.env.GITHUB_TOKEN);
 
 const rebase = () => {};
 
 const run = async () => {
     const context = await github.context;
-    owner = core.getInput('owner') || context.payload.repository.full_name.split('/')[0];
-    repo = core.getInput('repo') || context.payload.repository.full_name.split('/')[1];
-    console.log(github)
-    console.log(github.context.payload);
-    console.log(github.context.issue())
+    owner = context.payload.repository.full_name.split('/')[0];
+    repo = context.payload.repository.full_name.split('/')[1];
+    issue_number = context.payload.issue.number;
+    console.log(github.ref)
+    console.log(github.base_ref);
+    console.log(github.head_ref);
 };
 
 run();
